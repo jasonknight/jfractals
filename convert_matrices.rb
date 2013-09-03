94ecace8e480032cc30fa6581d9e0e3e0793f854
Jason Knight <jason@jason-knight-martin.com>
2013-09-03 11:34:40 -0400

    Converted Matrices

---
+++ convert_matrices.rb
@@ -0,0 +1,23 @@
+puts "var L = [];"
+12.times do |i|
+	puts "L[ #{i + 1 } ] = [];"
+	4.times do |j|
+		puts "L[ #{i + 1 } ][ #{j+1} ] = [];"
+	end
+end
+File.open('matrices.fi').each_line do |line|
+	lside, rside = line.split("=")
+	rside = rside.gsub(/[\d\.-]+D0/) {|m| " #{m.gsub("D0",'')} " }
+	rside = rside.gsub(/ALPHA\*\*\d+/) { |m| 
+		num = m.scan(/(\d+)/)
+		" Math.pow( ALPHA, #{num[0][0]} ) "
+	}
+	rside.gsub!(")/(",") / (")
+	rside.gsub!("*ALPHA","* ALPHA")
+	rside.gsub!("  "," ")
+	rside.gsub!("ALPHA","$ALPHA");
+	indices = lside.scan(/\L\((\d+),(\d+),(\d+)\)/)
+	indices = indices[0]
+	lside = "L[ #{indices[0]} ][ #{indices[1]} ][ #{indices[2]} ]"
+	puts "#{lside} = #{rside.strip};"
+end
